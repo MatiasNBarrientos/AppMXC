68d05d7a7f92064a88fe848d86b1134b9159b681
MatiasNBarrientos <matiasbarrientos537@gmail.com>
2025-05-06 15:13:09 -0300

    pantallas de inicio, login y de registro

--- src/app/(auth)/_layout.tsx
+++ src/app/(auth)/_layout.tsx
@@ -2,24 +2,17 @@ import { Tabs } from 'expo-router';
 import React from 'react';
 import { Platform } from 'react-native';
 
-
 const RootNavigation = () => {
   return (
-    
     <Tabs
       screenOptions={{
         headerShown: false,
-        tabBarStyle: Platform.select({
-          android: {
-            position: 'absolute',
-          },
-          default: {},
-        }),
+        tabBarStyle: { display: 'none' }, // Oculta la barra de navegación
       }}>
       <Tabs.Screen name="index" />
       <Tabs.Screen name="explore" />
     </Tabs>
-    
   );
 }
+
 export default RootNavigation;
