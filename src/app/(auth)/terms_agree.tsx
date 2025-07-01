761f08f4a45d9f8a6e43b7d229a071dd0e47d47f
Lucas Luque <lucasluqueencina@gmail.com>
2025-07-01 16:54:59 -0300

    Normalizado el header de terms_agree

--- src/app/(auth)/terms_agree.tsx
+++ src/app/(auth)/terms_agree.tsx
@@ -18,40 +18,44 @@ const TermsScreen = () => {
 
   return (
     <SafeAreaView style={[dynamicStyles.container, { backgroundColor: themeColors.background }]}>
-      <View style={[dynamicStyles.body, styles.container]}>
-        <TouchableOpacity onPress={() => router.back()} style={styles.backButton}>
-          <View style={[styles.circle, { backgroundColor: themeColors.background, borderColor: themeColors.text }]}>
+      <View>
+        <TouchableOpacity
+        style={[dynamicStyles.backButton]}
+        onPress={() => router.back()}>
+          <View style={[dynamicStyles.circle, { backgroundColor: themeColors.background, borderColor: themeColors.text }]}>
             <Ionicons name="arrow-back" size={24} color={themeColors.text} />
           </View>
         </TouchableOpacity>
 
-        <Text style={[styles.title, dynamicStyles.textLarge, { color: themeColors.primary }]}>
+        <Text style={[dynamicStyles.title, dynamicStyles.textLarge, { color: themeColors.primary }]}>
           Términos y Condiciones
         </Text>
 
-        <ScrollView
-          style={[styles.scroll, { backgroundColor: themeColors.background }]}
-          contentContainerStyle={{ paddingBottom: 20 }}
-          ref={scrollViewRef}
-        >
-          <Text style={[styles.text, dynamicStyles.baseText, { color: themeColors.text }]}>
-            {/* Texto largo de términos */}
-            L15.1 Gracias por visitar nuestra aplicación y registrarte como miembro.{"\n\n"}
-            15.2 Tu privacidad es importante para nosotros. Para protegerla mejor, estamos proporcionando
-            este aviso explicando nuestra política con respecto a la información que compartes con nosotros.
-            Esta política de privacidad se relaciona con la información que recopilamos en línea desde la
-            aplicación, recibida por correo electrónico, fax, teléfono, en persona o de cualquier otra manera,
-            y que retenemos y usamos para el propósito de brindarte servicios.{'\n\n'}
-            15.3 Al usar esta aplicación, aceptas los términos de esta política de privacidad.{'\n\n'}
-            15.4 Nos reservamos el derecho de actualizar esta política de privacidad de vez en cuando.
-            Notificaremos sobre cambios significativos enviando un aviso al correo electrónico principal
-            especificado en tu cuenta o colocando un aviso destacado en nuestra aplicación antes de que
-            los cambios entren en vigor.{'\n\n'}
-            15.5 Continuar usando la aplicación después de cualquier cambio constituye tu aceptación de la
-            nueva política de privacidad.{'\n\n'}
-            {/* Agrega más texto según sea necesario */}
-          </Text>
-        </ScrollView>
+        <View style={[dynamicStyles.body]}>
+          <ScrollView
+            style={[styles.scroll, { backgroundColor: themeColors.background }]}
+            contentContainerStyle={{ paddingBottom: 20 }}
+            ref={scrollViewRef}
+          >
+            <Text style={[styles.text, dynamicStyles.baseText, { color: themeColors.text }]}>
+              {/* Texto largo de términos */}
+              L15.1 Gracias por visitar nuestra aplicación y registrarte como miembro.{"\n\n"}
+              15.2 Tu privacidad es importante para nosotros. Para protegerla mejor, estamos proporcionando
+              este aviso explicando nuestra política con respecto a la información que compartes con nosotros.
+              Esta política de privacidad se relaciona con la información que recopilamos en línea desde la
+              aplicación, recibida por correo electrónico, fax, teléfono, en persona o de cualquier otra manera,
+              y que retenemos y usamos para el propósito de brindarte servicios.{'\n\n'}
+              15.3 Al usar esta aplicación, aceptas los términos de esta política de privacidad.{'\n\n'}
+              15.4 Nos reservamos el derecho de actualizar esta política de privacidad de vez en cuando.
+              Notificaremos sobre cambios significativos enviando un aviso al correo electrónico principal
+              especificado en tu cuenta o colocando un aviso destacado en nuestra aplicación antes de que
+              los cambios entren en vigor.{'\n\n'}
+              15.5 Continuar usando la aplicación después de cualquier cambio constituye tu aceptación de la
+              nueva política de privacidad.{'\n\n'}
+              {/* Agrega más texto según sea necesario */}
+            </Text>
+          </ScrollView>
+        </View>
       </View>
     </SafeAreaView>
   );
@@ -65,25 +69,10 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
     paddingTop: 56,
   },
-  backButton: {
-    position: 'absolute',
-    top: 12,
-    left: 8,
-    zIndex: 10,
-  },
-  circle: {
-    width: 40,
-    height: 40,
-    borderRadius: 20,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#fff',
-    borderWidth: 1,
-    borderColor: 'gray',
-    shadowColor: '#000',
-    shadowOpacity: 0.1,
-    shadowRadius: 2,
-    elevation: 2,
+  header: {
+    flexDirection: "row",
+    alignItems: "flex-start",
+    backgroundColor:"red"
   },
   scroll: {
     flex: 1,
